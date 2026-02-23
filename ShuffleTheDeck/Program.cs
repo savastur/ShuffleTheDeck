8bd322b55c75960cef05e4681a4a0e7944767681
Sturm <savagesturm@gmail.com>
2026-02-23 08:47:57 -0700

    Add project files.

---
+++ ShuffleTheDeck/Program.cs
@@ -0,0 +1,18 @@
+﻿/* To Do
+ * [] Create an array of each suit
+ * [] Diamnond suit
+ * [] Clubs suit
+ * [] Hearts suit
+ * [] Spades suit
+ * [] Create a display to show 52 cards 
+*/
+namespace ShuffleTheDeck
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
