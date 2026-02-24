c60f0659c744eb88225bfb43a567279f4fb8623f
Sturm <savagesturm@gmail.com>
2026-02-24 08:56:19 -0700

    Creating the display for interface.

--- ShuffleTheDeck/Program.cs
+++ ShuffleTheDeck/Program.cs
@@ -4,15 +4,32 @@
  * [] Clubs suit
  * [] Hearts suit
  * [] Spades suit
+ * [] 
  * [] Create a display to show 52 cards 
 */
+using System.ComponentModel.DataAnnotations;
+
 namespace ShuffleTheDeck
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Display();
+            Console.Read();
         }
+        static void Display()
+        {
+            string location = "XX";
+            for (int row = 0; row < 52; row++)
+            {
+                for (int column = 0; column < 13; column++)
+                {
+                    Console.Write(location.PadLeft(4) + " |");
+                }
+                    Console.WriteLine();
+            }
+        }
+       
     }
 }
